596124a5902d22d18f2223fba20559ae09e6e416
Srishti Negi <negisrishti2608@gmail.com>
2026-06-19 11:38:42 +0530

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main ()
+ {
+for(int i = 0; i <= 10; i+=2) {
+std::cout << i << '\n';
+}
+
+
+// ++ adds one number every time after i      (increases)
+// -- subtracts one number each time after i   (decreases)
+// i <=     condition the loop keeps running as long as the number is less than or eqaul to i
+    return 0;
+}
